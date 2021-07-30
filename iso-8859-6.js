87fc4b9e323d6a3349875c326bd6b2034de513d7
Mathias Bynens <mathias@qiwi.be>
2021-07-30 12:23:40 +0200

    Release v1.0.2

--- iso-8859-6.js
+++ iso-8859-6.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/iso-8859-6 v1.0.1 by @mathias | MIT license */
+/*! https://mths.be/iso-8859-6 v1.0.2 by @mathias | MIT license */
 ;(function() {
 
 	var object = {};
@@ -115,7 +115,7 @@
 			'iso_8859-6',
 			'iso_8859-6:1987'
 		],
-		version: '1.0.1',
+		version: '1.0.2',
 	};
 
 	module.exports = iso88596;
